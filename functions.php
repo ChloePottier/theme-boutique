980ca1abca0b211a00cb1158583229197bc1dca5
Chloepottier <chloe.pottier@free.fr>
2021-06-15 16:01:15 +0200

    premières fonctionnalités

--- functions.php
+++ functions.php
@@ -1,6 +1,21 @@
 <?php
 
 function wp_styles_scripts(){
+
     wp_enqueue_style('style', get_stylesheet_uri());
 
 }
+add_action('wp_enqueue_scripts', 'wp_styles_scripts');
+//Ajouter le logo
+function themename_custom_logo_setup() {
+    $defaults = array(
+    'height'      => 150,
+    'width'       => 150,
+    'flex-width'  => true,
+    'header-text' => array( 'site-title', 'site-description' )
+    );
+    add_theme_support( 'custom-logo', $defaults );
+   }
+   add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
+// Ajouter la prise en charge des images mises en avant
+add_theme_support( 'post-thumbnails' );
--- header.php
+++ header.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php get_template_part('google.php');?>
+    <?php //get_template_part('google.php');?>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
