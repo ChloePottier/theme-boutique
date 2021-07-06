ad4520a2364c72aeb257537a538b34c99ef9155e
Chloepottier <chloe.pottier@free.fr>
2021-07-06 15:38:34 +0200

    alignement menus footer

--- footer.php
+++ footer.php
@@ -4,23 +4,23 @@
             <div class='row fw-bold fst-italic'>
                 <div class='col-12 col-md-6 text-center text-md-start nav-footer' >
                     <h3 class='fw-bold text-uppercase fs-6'>Service client</h3>
-                    <?php wp_nav_menu( array( 'theme_location' => 'subfooter-menu','menu_class' => 'menu-subfooter fs-7','container' => 'nav' ) ); ?>
+                    <?php wp_nav_menu( array( 'theme_location' => 'subfooter-menu','menu_class' => 'menu-subfooter p-0 fs-7','container' => 'nav' ) ); ?>
                 </div>
                 <div class='col-12 col-md-6 text-center text-md-end'></div>
             </div>
         </div>
     </div>
-    <footer class='site-footer container-fluid py-3 bg-white position-relative border-top nav-footer' role='contentinfo'>
+    <footer class='site-footer container-fluid py-3 bg-white position-relative border-top' role='contentinfo'>
         <div class='container pt-4'>
-            <div class='row'>
+            <div class='row flex-md-row-reverse'>
+                <div class='col-12 col-md-6 fs-2 nav-footer'>
+                    <?php wp_nav_menu( array( 'theme_location' => 'footer-menu','menu_class' => 'menu-footer p-0 d-flex flex-row justify-content-center justify-content-md-end','container' => 'nav' ) ); ?>
+                </div>
                 <div class='col-12 col-md-6 text-center text-md-start' id='copyrightinfo' >
                     <p class='mb-0'><a href='<?php echo get_home_url(); ?>'class='fw-bold text-black'><?php bloginfo('name'); ?></a> - Copyright © 2021</p>
                     <p class='akaleya'>Site éco-conçus par <a href='https://akaleya.fr' target='_blank' rel='noreferrer'class='fw-600'>Akaleya</a></p>
                 </div>
-                <div class='col-12 col-md-6 text-center text-md-end fs-2'>
-                <?php wp_nav_menu( array( 'theme_location' => 'footer-menu','menu_class' => 'menu-footer','container' => 'nav' ) ); ?>
-
-                </div>
+                
             </div>
         </div>
     </footer>
