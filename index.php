7fc012f5823ee377f01cb76c045f9b30885a4577
Chloepottier <chloe.pottier@free.fr>
2021-07-07 13:06:53 +0200

    faq

--- index.php
+++ index.php
@@ -4,16 +4,15 @@
  *
  * @package akaleyaboutique
  */
-
 get_header(); ?>
-
-<div id='test' class='content-area'>
-    <main id='main' class='site-main ' role='main'>
-
-    <?php 
-    get_template_part('template-parts/content/content','home');?>
-
-
-    </main><!-- #main -->
-</div><!-- #primary -->
+    <?php if (have_posts()) :
+            while (have_posts()) : the_post(); ?>
+            <div id='post-<?php echo $post->ID; ?>' class='content-area container-fluid py-5 bg-white position-relative'>
+                <main id='main' class='site-main container position-relative' role='main'>
+                    <h1 class='pb-3'><?php the_title(); ?></h1>
+                    <?php the_content(); ?>            
+                </main>
+            </div>
+    <?php endwhile;
+        endif; ?>
 <?php get_footer(); ?>
--- template-parts/content/content-home.php
+++ template-parts/content/content-home.php
@@ -1,6 +1,6 @@
 <?php if (have_posts()) :
             while (have_posts()) : the_post(); ?>
-                    <section class='row' id='contact'>
+                    <section class='row'>
                         <div class='col-12'>
                         <h1><?php the_title(); ?></h1>
                             <?php the_content(); ?> 
