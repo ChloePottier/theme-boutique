6501014ecd178f2210729533adb795ad0dbaa5ac
ChloePottier <chloe.pottier@free.fr>
2021-09-09 17:12:16 +0200

    modif référence UGS 3

---
+++ woocommerce/single-product/meta.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Single Product Meta
+ */
+if (! defined('ABSPATH')) {
+    exit;
+}
+global $product; ?>
+<div class='product_meta'>
+	<?php do_action('woocommerce_product_meta_start');
+	if (wc_product_sku_enabled() && ($product->get_sku() || $product->is_type('variable'))) : ?>
+	<span class='sku_wrapper fw-bold'><?php esc_html_e('Référence :', 'woocommerce'); ?>
+		<span class='sku fw-normal'><?php echo ($sku = $product->get_sku()) ? $sku : esc_html__('Veuillez sélectionner une taille', 'woocommerce'); ?></span></span>
+	<?php endif;
+	do_action('woocommerce_product_meta_end'); ?>
+</div>
